27e35f2be9cf99c20e114d1dead17348d5ee791d
adamjed34 <adamj34@wp.pl>
2023-10-23 19:56:56 +0200

    homework

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,93 +1,117 @@
 const express = require("express");
-
-const Joi = require("joi");
-
-const router = express.Router();
-
 const {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
   updateContact,
-} = require("../../models/contactFunctions");
+} = require("../../services/contacts.service");
+const Joi = require("joi");
+const { validateContact } = require("../../schema/contacts.schema");
 
-const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
-
-const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-});
+const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const contacts = await listContacts();
-    res.json(contacts);
+    res.status(200).json({ status: "Success", code: 200, contacts });
   } catch (error) {
-    next(error);
+    console.error(error.messsage);
+    res
+      .status(404)
+      .json({ status: "Not found", code: 404, message: "No data found" });
   }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
   try {
-    const contact = await getContactById(contactId);
+    const contact = await getContactById(req.params.contactId);
     if (!contact) {
-      return res.status(404).json({ message: "Not found" });
+      res
+        .status(404)
+        .json({ status: "Not Found", code: 404, message: "Contact not found" });
+    } else {
+      res.status(200).json({
+        status: "Success",
+        code: 200,
+        message: `Contact with id: ${req.params.contactId} found`,
+        contact,
+      });
     }
-    res.json(contact);
   } catch (error) {
-    next(error);
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
   }
 });
 
 router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const { error } = createContactSchema.validate(req.body);
-
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-
   try {
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
+    const validatedContact = validateContact(req.body);
+
+    if (validatedContact.error) {
+      res.status(400).json({
+        status: "Bad request",
+        code: 400,
+        message: "Missing required field",
+      });
+    } else {
+      const newContact = await addContact(req.body);
+      res.status(201).json({
+        status: "Created",
+        code: 201,
+        message: "Success! New contact added",
+        newContact,
+      });
+    }
+  } catch {
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
   }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
   try {
-    await removeContact(contactId);
-    res.json({ message: "Contact deleted" });
+    const filteredList = await removeContact(req.params.contactId);
+    if (filteredList) {
+      res.status(204).json({
+        status: "No Content",
+        code: "204",
+        message: "Contact successfully deleted",
+        filteredList,
+      });
+    } else {
+      res
+        .status(404)
+        .json({ status: "Not found", code: 404, message: "Contact not found" });
+    }
   } catch (error) {
-    next(error);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
   }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-
-  const { error } = updateContactSchema.validate(req.body);
-
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-
-  const { name, email, phone } = req.body;
-
   try {
-    const updatedContact = await updateContact(contactId, req.body);
-    res.json(updatedContact);
+    const { contactId } = req.params;
+
+    const validatedContact = validateContact(req.body);
+
+    if (validatedContact.error) {
+      res.status(400).json({
+        status: "Bad request",
+        code: 400,
+        message: "Data validation: missing fields",
+      });
+    } else {
+      const updatedList = await updateContact(contactId, req.body);
+      res.status(200).json({
+        status: "OK",
+        code: "200",
+        message: "Contact updated",
+        updatedList,
+      });
+    }
   } catch (error) {
-    next(error);
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
   }
 });
 
